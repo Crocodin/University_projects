888fb1f031c1507cb060891f8add4efc8f7a3f28
Crocodin <parmac.codrin@gmail.com>
2025-04-02 17:00:40 +0300

    liked list

---
+++ semester 2/SDA/TAD Coada cu Prioritati/Proiect C++/CP.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <utility>
+
+using namespace std;
+
+typedef int TElem;
+typedef int TPrioritate;
+
+typedef std::pair<TElem, TPrioritate> Element;
+
+typedef bool (*Relatie)(TPrioritate p1, TPrioritate p2);
+
+class CP {
+private:
+	/// linked list
+	struct Node {
+		Element info { 0, 0 };
+		Node* next = nullptr;
+		Node* prev = nullptr;
+	};
+
+	Node* head = nullptr;
+	Node* tail = nullptr;
+
+	const Relatie relatie;
+
+public:
+	//constructorul implicit
+	explicit CP(Relatie r);
+
+	//adauga un element in CP
+	void adauga(TElem e, TPrioritate p);
+
+	//acceseaza elementul cel mai prioritar in raport cu relatia de ordine
+	//arunca exceptie daca CP e vida
+	Element element()  const;
+
+	//sterge elementul cel mai prioritar si il returneaza
+	//arunca exceptie daca CP e vida
+	Element sterge();
+
+	//verifica daca CP e vida;
+	bool vida() const;
+
+	// destructorul cozii
+	~CP();
+
+};
--- semester 2/SDA/TAD Colectie/Proiect C++/Colectie/IteratorColectie.cpp
+++ semester 2/SDA/TAD Colectie/Proiect C++/Colectie/IteratorColectie.cpp
@@ -4,22 +4,23 @@
 
 #include "Colectie.h"
 
-
+// BC = WC = theta(1)
 IteratorColectie::IteratorColectie(const Colectie& c): col(c) {
 	this->position = 0;
 }
 
+// BC = WC = theta(1)
 void IteratorColectie::prim() {
 	this->position = 0;
 }
 
-
+// BC = WC = theta(1)
 void IteratorColectie::urmator() {
 	if (!this->valid()) throw std::runtime_error("Invalid call of IteratorColectie::urmator()");
 	this->position++;
 }
 
-
+// BC = WC = theta(1)
 bool IteratorColectie::valid() const {
 	if (this->position < this->col.position_current_size or this->position < 0)
 		return true;
@@ -27,7 +28,7 @@ bool IteratorColectie::valid() const {
 }
 
 
-
+// BC = WC = theta(1)
 TElem IteratorColectie::element() const {
 	if (!this->valid()) throw std::out_of_range("Invalid iterator, out of range");
 	return this->col.distinct[this->col.position[this->position]];
