a30d84b6ad5e04511880f9f246b630614e24a431
Crocodin <parmac.codrin@gmail.com>
2025-05-19 16:47:00 +0300

    SDA - hash table

--- semester 2/SDA/TAD Multime/Proiect C++/Multime/IteratorMultime.cpp
+++ semester 2/SDA/TAD Multime/Proiect C++/Multime/IteratorMultime.cpp
@@ -4,13 +4,13 @@
 
 #include "Multime.h"
 
-
+/// theta(1)
 IteratorMultime::IteratorMultime(const Multime& m) : multime(m){
 	this->currentElementIndex = NULL_TELEM;
 	this->prim();
 }
 
-
+/// O(m) { m - capacity }
 void IteratorMultime::prim() {
 	for (int i = 0; i < this->multime.elements.capacity; ++ i)
 		if (this->multime.elements[i] != NULL_TELEM) {
@@ -20,9 +20,9 @@ void IteratorMultime::prim() {
 	this->currentElementIndex = NULL_TELEM;
 }
 
-
+/// O(m) { m - capacity }
 void IteratorMultime::urmator() {
-	if (this->currentElementIndex == NULL_TELEM) throw std::out_of_range("IteratorMultime is empty");
+	if (!this->valid()) throw std::out_of_range("IteratorMultime is empty");
 	for (int i = this->currentElementIndex + 1; i < this->multime.elements.capacity; ++ i)
 		if (this->multime.elements[i] != NULL_TELEM) {
 			this->currentElementIndex = i;
@@ -31,12 +31,24 @@ void IteratorMultime::urmator() {
 	this->currentElementIndex = NULL_TELEM;
 }
 
-
+/// theta(1)
 TElem IteratorMultime::element() const {
-	if (this->currentElementIndex == NULL_TELEM) throw std::out_of_range("IteratorMultime is empty");
+	if (!this->valid()) throw std::out_of_range("IteratorMultime is empty");
 	return this->multime.elements[this->currentElementIndex];
 }
 
+/// theta(1)
 bool IteratorMultime::valid() const {
 	return this->currentElementIndex != NULL_TELEM;
+}
+
+/// O(m) { m - nr de elem }
+void IteratorMultime::anterior() {
+	if (!this->valid()) throw std::out_of_range("IteratorMultime is invalid");
+	for (int i = this->currentElementIndex - 1; i >= 0; -- i)
+		if (this->multime.elements[i] != NULL_TELEM) {
+			this->currentElementIndex = i;
+			return;
+		}
+	this->currentElementIndex = NULL_TELEM;
 }
--- semester 2/SDA/TAD Multime/Proiect C++/Multime/IteratorMultime.h
+++ semester 2/SDA/TAD Multime/Proiect C++/Multime/IteratorMultime.h
@@ -19,17 +19,21 @@ private:
 
 public:
 
-		//reseteaza pozitia iteratorului la inceputul containerului
-		void prim();
+	//reseteaza pozitia iteratorului la inceputul containerului
+	void prim();
 
-		//muta iteratorul in container
-		// arunca exceptie daca iteratorul nu e valid
-		void urmator();
+	//muta iteratorul in container
+	// arunca exceptie daca iteratorul nu e valid
+	void urmator();
 
-		//verifica daca iteratorul e valid (indica un element al containerului)
-		bool valid() const;
+	//verifica daca iteratorul e valid (indica un element al containerului)
+	bool valid() const;
 
-		//returneaza valoarea elementului din container referit de iterator
-		//arunca exceptie daca iteratorul nu e valid
-		TElem element() const;
+	//returneaza valoarea elementului din container referit de iterator
+	//arunca exceptie daca iteratorul nu e valid
+	TElem element() const;
+
+	///modifica elementul curent referit de iteratoru ca fiind elementul anterior din multime, sau daca
+	///elementul este primul iteratorul devine invalid
+	void anterior();
 };
