4cb67c1d427590a60715e188ab7d4e0be2b52972
Crocodin <parmac.codrin@gmail.com>
2025-05-24 20:17:58 +0300

    added coments

--- semester 2/OOP/shop_store/GUI/Cos/observer.hpp
+++ semester 2/OOP/shop_store/GUI/Cos/observer.hpp
@@ -3,24 +3,38 @@
 
 #include <vector>
 
+/// Interface for observer objects
+/// Classes implementing this interface must define the `update` method
 class Observer {
 public:
+	/// Called when the subject notifies its observers of a change
 	virtual void update() = 0;
+
+	/// Virtual destructor for safe deletion through base pointer
 	virtual ~Observer() = default;
 };
 
+/// Subject class that maintains and notifies a list of observers
+/// Observers can register to be notified when a change occurs in the subject
 class Subject {
 private:
-	std::vector<Observer*> observers;
+	std::vector<Observer*> observers;		 ///< List of registered observers
+
 public:
+	/// Adds an observer to the list
+	/// :param observer: pointer to the observer to add
 	void addObserver(Observer* observer) {
 		observers.push_back(observer);
 	}
 
+	/// Removes an observer from the list
+	/// :param observer: pointer to the observer to remove
 	void removeObserver(Observer* observer) {
-		std::erase(observers,observer);
+		std::erase(observers, observer);
 	}
 
+	/// Notifies all registered observers of a change
+	/// Calls the `update` method on each observer
 	void notifyObservers() const {
 		for (Observer* observer : observers) {
 			observer->update();
@@ -28,4 +42,4 @@ public:
 	}
 };
 
-#endif //OBSERVER_HPP
+#endif // OBSERVER_HPP
