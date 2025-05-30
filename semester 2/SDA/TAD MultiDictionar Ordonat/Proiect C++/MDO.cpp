413609d8ebe43f752ec3c2c803555a229c499101
Crocodin <parmac.codrin@gmail.com>
2025-05-30 17:27:54 +0300

    sda - last commit ever

--- semester 2/SDA/TAD MultiDictionar Ordonat/Proiect C++/MDO.cpp
+++ semester 2/SDA/TAD MultiDictionar Ordonat/Proiect C++/MDO.cpp
@@ -1,17 +1,15 @@
-#include "IteratorMDO.h"
 #include "MDO.h"
-#include <iostream>
+#include "IteratorMDO.h"
 #include <vector>
 #include <algorithm>
-
-#include <exception>
 using namespace std;
 
+/// theta (1)
 MDO::MDO(Relatie r) : cmp(r) {
 	this->root = nullptr;
 }
 
-
+/// O(h)
 void MDO::adauga(TCheie c, TValoare v) {
 	numberOfElements++;
 
@@ -39,6 +37,7 @@ void MDO::adauga(TCheie c, TValoare v) {
 	else parent->right = current;
 }
 
+/// O(h)
 vector<TValoare> MDO::cauta(TCheie c) const {
 	if (this->root == nullptr) return {};
 	Node *current = this->root;
@@ -53,11 +52,13 @@ vector<TValoare> MDO::cauta(TCheie c) const {
 	return {};
 }
 
+/// theta(max(h, m) { where m is the number of elements with that key }
 bool MDO::sterge(TCheie c, TValoare v) {
 	if (root == nullptr) return false;
 	Node *current = this->root, *parent = nullptr;
 	do {
 		if (current->cheie == c) {
+			/// this operation, i'll have to gues, it O(m) { where m is the number of elements with that key }
 			auto it = std::find(current->valori.begin(), current->valori.end(), v);
 			if (it != current->valori.end()) {
 				current->valori.erase(it);
@@ -124,18 +125,94 @@ bool MDO::sterge(TCheie c, TValoare v) {
 	return false;
 }
 
+/// theta (1)
 int MDO::dim() const {
 	return numberOfElements;
 }
 
+/// theta (1)
 bool MDO::vid() const {
 	return this->root == nullptr;
 }
 
+/// theta (1)
 IteratorMDO MDO::iterator() const {
 	return IteratorMDO(*this);
 }
 
+/// theta(h)
+void MDO::deleteRec(Node* node) {
+	if (node == nullptr)
+		return;
+	deleteRec(node->left);
+	deleteRec(node->right);
+	delete node;
+}
+
+/// theta(h)
 MDO::~MDO() {
-	/// todo recursvide delete
+	deleteRec(this->root);
+}
+
+TValoare maxTValoare(TValoare a, TValoare b) {
+	if (a > b) return a;
+	return b;
+}
+
+TValoare minTValoare(TValoare a, TValoare b) {
+	if (a < b) return a;
+	return b;
+}
+
+/*
+ * subprogram difRec(d, curent, cmp) este:
+ *		{ d este un MDO }
+ *		{ cmp este o functie de comparare }
+ *		{ curent ^Nod }
+ *		daca curent = NIL:
+ *			daca cmp = min atunci:
+ *				difRec <- -inf
+ *			altfel: difRec <- +inf
+ *		sf.daca
+ *
+ *		loacl <- [curent].valori[0]
+ *		pentru nod in valori:
+ *			loacl <- cmp(local, nod)
+ *
+ *		stanga <- difRec([curent].stanga, cmp)
+ *		dreapta <- difRec([curent].dreapta, cmp)
+ *		difRec <- cmp(local, cmp(stanga, dreapta))
+ *	sf.subprogram
+ *
+ * subprogram diferentaValoareMaxMin(d, cmp) este:
+ *		{ d este un MDO }
+ *		daca d.vid() atunci:
+ *			diferentaValoareMaxMin <- -1;
+ *		sf. daca
+ *
+ *		min <- difRec(d, d.root, min)
+ *		max <- difRec(d, d.root, max)
+ *		diferentaValoareMaxMin <- (max - min)
+ * sf.subprogram
+ */
+
+/// theta (n)
+TValoare MDO::searchCMP(MDO::Node* current, const searchF cmp) const{
+	if (current == nullptr)
+		return cmp == minTValoare ? 10000 : -10000;
+
+	TValoare local = current->valori[0];
+	for (int i = 1; i < current->valori.size(); ++i) {
+		local = cmp(local, current->valori[i]);
+	}
+
+	TValoare left = searchCMP(current->left, cmp);
+	TValoare right = searchCMP(current->right, cmp);
+
+	return cmp(local, cmp(left, right));
+}
+
+int MDO::maxMinDiff() const {
+	if (this->root == nullptr) return -1;
+	return searchCMP(this->root, maxTValoare) - searchCMP(this->root, minTValoare);
 }
